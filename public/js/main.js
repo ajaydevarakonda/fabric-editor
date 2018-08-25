5b5296a3588df87b5659beb2fce16304e4790c0b
Ramachandra Junior <ramachandrajr3@gmail.com>
2018-08-25 11:43:14 +0530

    Add undo and redo features.

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,74 @@
 'use strict'
 
 var canvas = new fabric.Canvas('c');
-var canvasAsJSONString;     
+var canvasAsJSONString; // using canvasAsJSON string as a global variable for easy debugging, no probs even if not made local.
+
+function objectIsEqual(obj1, obj2) {
+  return (JSON.stringify(obj1) === JSON.stringify(obj2));
+}
+
+function initUndoList() {
+  let emptyArray = [];
+  sessionStorage.setItem("undoList", JSON.stringify(emptyArray));
+  sessionStorage.setItem("undoListCurrentIndex", -1);
+  sessionStorage.setItem("undoListLength", 0);  
+}
+
+function addToUndoList(imageAsJSON) {
+  // take from undo list
+  let undoList = JSON.parse(sessionStorage.getItem("undoList"));
+  undoList.push(imageAsJSON);
+  // increment length and current index
+  let currentIndex = sessionStorage.getItem("undoListCurrentIndex");
+  let listLength = sessionStorage.getItem("undoListLength");
+  currentIndex++;
+  listLength++;
+  sessionStorage.setItem("undoListCurrentIndex", currentIndex);
+  sessionStorage.setItem("undoListLength", listLength);
+  // write back to undo list  
+  sessionStorage.setItem("undoList", JSON.stringify(undoList));
+}
+
+function getFromUndoList(index) {
+  // get undo list first.
+  let undoList = JSON.parse(sessionStorage.getItem("undoList"));
+  // then get item at index
+  return undoList[index];
+}
+
+function undo(canvas) {
+  // get current index and list length
+  let currentIndex = parseInt(sessionStorage.getItem("undoListCurrentIndex"), 10);
+  let listLength = parseInt(sessionStorage.getItem("undoListLength"), 10);  
+  
+  // check if the current index is less than zero, if so we can't undo
+  if (currentIndex <= 0 || listLength < 1) {
+    return;
+  }
+  
+  // reduce current index
+  // draw previous scene on to the canvas
+  let previousCanvasJSONString = getFromUndoList(--currentIndex);
+  // write the current index to sessionStorage
+  sessionStorage.setItem("undoListCurrentIndex", currentIndex);
+  canvas.loadFromJSON(previousCanvasJSONString, canvas.renderAll.bind(canvas));
+}
+
+function redo(canvas) {
+  // get current index and list length
+  let currentIndex = parseInt(sessionStorage.getItem("undoListCurrentIndex"), 10);
+  let listLength = parseInt(sessionStorage.getItem("undoListLength"), 10);  
+
+  // check if the current index is less than zero, if so we can't undo
+  if (currentIndex >= listLength) {
+    return;
+  }
+  // draw previous scene on to the canvas
+  let previousCanvasJSONString = getFromUndoList(currentIndex);
+  // write the reduced current index to sessionStorage
+  sessionStorage.setItem("undoListCurrentIndex", ++currentIndex);
+  canvas.loadFromJSON(previousCanvasJSONString, canvas.renderAll.bind(canvas));
+}
 
 function uploadImageToCanvas(canvas, imageFileObject) {
   let imgReader = new FileReader();
@@ -14,7 +81,7 @@ function uploadImageToCanvas(canvas, imageFileObject) {
     imgObj.onload = function () {
       var image = new fabric.Image(imgObj);
       image.set({
-            cornersize:10
+        cornersize:10
       });
       canvas.centerObject(image);
       canvas.add(image);
@@ -24,6 +91,13 @@ function uploadImageToCanvas(canvas, imageFileObject) {
   imgReader.readAsDataURL(imageFileObject);
 }
 
+// script starts its real run here
+// -------------------------------
+initUndoList();
+
+// TODO: upload image to canvas on button click
+// onchange on the image may not work when you select the same image once again,
+// or when you'd just like to insert a picture once again.
 // uploading image
 document.getElementById('image-input').onchange = function(e) {
   for (let i = 0; i < e.target.files.length; ++i) {
@@ -33,7 +107,7 @@ document.getElementById('image-input').onchange = function(e) {
 
 // inserting text
 document.getElementById('insert-txt').onclick = function(e) {
-  let textbox = new fabric.Textbox('Lorum ipsum dolor sit amet', {
+  let textbox = new fabric.Textbox('Your text here', {
     left: 50,
     top: 50,
     width: 150,
@@ -76,3 +150,25 @@ document.getElementById('reopen').onclick = function(e) {
   saveDataRequest.open("GET", "/reopen", true);
   saveDataRequest.send();
 }
+
+// undo
+document.getElementById("undo").addEventListener("click", function() {
+  undo(canvas);
+})
+
+// redo
+document.getElementById("redo").addEventListener("click", function() {
+  redo(canvas);
+})
+
+// record mouse up events, and 
+document.getElementById("wrapper").addEventListener("mouseup", function (event) {
+  // get the previous object
+  // check if it is the same as the current object
+  // get the whole image as json
+  canvasAsJSONString = JSON.stringify(canvas.toDatalessJSON());
+  if (canvasAsJSONString == getFromUndoList(sessionStorage.getItem("undoListCurrenIndex"))) { console.log("same as before");
+  ;return; }
+  // and then add it to the session storage array
+  addToUndoList(canvasAsJSONString);
+});
